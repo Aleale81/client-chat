f63b9794b14451addea9000d91a9c2d2deee5fd6
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-01-26 15:44:10 +0100

    connection successful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:4000');
 
 function App() {
   return (
     <div className="App">
-<h1>CHAT</h1>
+       <h1>CHAT</h1>
+
     </div>
   );
 }
